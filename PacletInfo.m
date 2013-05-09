2f87ea5eb06545266ae3e401886bf1c1318a3d0e
Kenta Takagaki <kentaroh.takagaki+git@gmail.com>
2013-05-09 23:23:09 +0200

    Documentation initialized

--- PacletInfo.m
+++ PacletInfo.m
@@ -1,10 +1,10 @@
 (* Paclet Info File *)
 
-(* created 2013/05/06*)
+(* created 2013/05/09*)
 
 Paclet[
     Name -> "FFmpegM",
-    Version -> "0.2.0",
+    Version -> "0.3.0",
     MathematicaVersion -> "6+",
     Creator -> "Kenta Takagaki",
     Extensions -> 
